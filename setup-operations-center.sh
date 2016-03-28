4305d23dfc31df7c14037867ee4722d4f09e277c
Nicolas De Loof <nicolas.deloof@gmail.com>
2016-03-28 10:50:07 +0200

    unnecessary for BYOL

--- setup-operations-center.sh
+++ setup-operations-center.sh
@@ -13,30 +13,9 @@ set -e
 #   setup-operations-center.sh <number_of_masters> <dns_domain_name> <template_root_url>
 #
 
-
-function finish {
-  # delete myslef. We don't want to assist people in hacking our licensing stuff
-  rm -- "$0"
-}
-trap finish EXIT
-
-# FIXME refresh VM image ?
-apt-get update ; apt-get install -y --only-upgrade walinuxagent
-
 masters=$1
 domain=$2
 rooturl=$3
-
-# unique ID of this VM
-uuid=`dmidecode -s system-uuid`
-# Allow jenkins user to run `sudo dmidecode`
-echo "jenkins-oc ALL=(ALL) NOPASSWD: /usr/sbin/dmidecode" >> /etc/sudoers.d/dmidecode
-
-apt-get install -y xml2
-
-# Extract licensing metadata from CloudInit config and inject as encrypted file to discourage (bad) hackers
-cat /var/lib/waagent/ovf-env.xml | xml2 | sed -n 's/^.*CustomData=//p' | base64 --decode | openssl enc -des3 -k $uuid  -out /var/lib/jenkins-oc/license.des
-
 
 INIT=/var/lib/jenkins-oc/init.groovy.d/oc-init-masters.groovy
 curl $rooturl/oc-init-masters.groovy -o $INIT
